0490cfcff2cdb119a584c2b4de43c3c6f00b6b36
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-01 08:45:02 +0100

    elearning

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -40,8 +40,6 @@ class AuthenticatedSessionController extends Controller
             'password' => 'required',
         ]);
 
-
-
         if (Auth::attempt($request->only('email', 'password'))) {
             // Vérification du rôle après connexion
             if (auth()->user()->hasRole(UserRole::ADMIN)) {
@@ -51,24 +49,6 @@ class AuthenticatedSessionController extends Controller
             } else {
                 return redirect()->route('student.dashboard');
             }
-            } elseif (auth()->user()->hasRole(UserRole::TEACHER)) {
-                return redirect()->route('teacher.dashboard');
-            } else {
-                return redirect()->route('student.dashboard');
-            }
-
-            // $user = auth()->user();
-
-            // if ($user->role === 'admin') {
-            //     return redirect()->route('admin.dashboard'); // Assurez-vous d'avoir cette route
-            // } elseif ($user->role === 'student') {
-            //     return redirect()->route('student.dashboard'); // Assurez-vous d'avoir cette route
-            // } elseif ($user->role === 'teacher') {
-            //     return redirect()->route('teacher.dashboard'); // Assurez-vous d'avoir cette route
-            // }
-        
-            // return abort(403); 
-
         }
 
         return back()->withErrors([
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs'
+import App from './components/App.vue'
+
 
 window.Alpine = Alpine;
 createApp(App).mount('#app');
