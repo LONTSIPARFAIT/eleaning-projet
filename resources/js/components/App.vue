54f8efe45e8b04e8286687e52485081ca811739f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-01 08:35:09 +0100

    elearning

---
+++ resources/js/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello from Vue!</h1>
+    <p>Bienvenue dans votre application Vue.js intégrée avec Laravel.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+
+p {
+  font-size: 1.2em;
+}
+</style>
