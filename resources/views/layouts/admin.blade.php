5327561034e55be541634c37df04361157c13011
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-05 15:02:55 +0100

    elearning

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,7 +11,7 @@
 <body>
     @include('layouts.navigation')
     @yield('content')
-    <footer class="bg-teal-600 text-white p-4 text-center">
+    <footer class="bg-red-600 text-white p-4 text-center">
         <p>&copy; 2024 Perfect-coding - Tous droits réservés-2024</p>
         <div>
             <a href="#privacy" class="text-white hover:underline">Politique de confidentialité</a> |
