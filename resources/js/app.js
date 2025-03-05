0d01dce6cec9044807a7c14587c066ce3cf5087e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-05 14:31:54 +0100

    elearning

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
 
+
 import Alpine from 'alpinejs'
-import App from './components/App.vue'
+// import App from './components/App.vue'
+// import { createApp } from 'vue';
 
 
 window.Alpine = Alpine;
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
 
 Alpine.start();
