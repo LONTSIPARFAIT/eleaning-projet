45c5dccc61bcfb3c361ab4cb1abc1f845d70d96f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-26 15:50:36 +0100

    elearning

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -12,30 +12,30 @@ class TeacherController extends Controller
     {
         // Récupérer les devoirs à évaluer
         //    $assignments = Assignment::whereIn('course_id', $courses->pluck('id'))->where('status', 'pending')->get();
-        
+
         // Récupérer les ressources pédagogiques
         //    $resources = Resource::where('teacher_id', $teacher->id)->get();
-        
+
         // Récupérer l'enseignant authentifié
         $teacher = Auth::user();
-        
+
         // Récupérer les cours enseignés par l'enseignant
         $cours = Cour::where('teacher_id', $teacher->id)->get();
 
         // Récupérer les cours enseignés par l'enseignant
         $courses = $teacher->cours;
-        
+
         // Récupérer le nombre d'étudiants associés à ces cours
-        $studentsCount = $courses->flatMap(function($cour) {
+        $studentsCount = $courses->flatMap(function ($cour) {
             return $cour->students; // Assurez-vous que vous avez une relation students dans Cour
-        })->unique('id')->count();   
-        
+        })->unique('id')->count();
+
 
         // Nombre total de cours
         $newCourses = Cour::orderBy('created_at', 'desc')->take(5)->get(); // Derniers cours créés
-            
+
         // Retourner la vue avec les données nécessaires
-        return view('teacher.dashboard', compact('courses','studentsCount', 'newCourses', 'cours'));
+        return view('teacher.dashboard', compact('courses', 'studentsCount', 'newCourses', 'cours'));
     }
 
     // public function index()
@@ -57,4 +57,3 @@ class TeacherController extends Controller
     //     ));
     // }
 }
-
