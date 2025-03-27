b916ee0656e1efa627f20af3582935d969027050
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-27 13:30:35 +0100

    elearning

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -149,7 +149,13 @@
                 @endauth
             @endif
         </div>
-
+                        <!-- Dans le dropdown, après "Se Déconnecter" -->
+<div class="mt-3">
+    <button @click="document.documentElement.classList.toggle('dark')" class="w-full text-left px-4 py-2 text-sm text-blue-800 dark:text-orange-500 hover:bg-orange-200 dark:hover:bg-gray-700 flex items-center gap-2">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+        {{ __('Mode Sombre') }}
+    </button>
+</div>
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-blue-200 dark:border-orange-600">
             <div class="px-4">
@@ -173,6 +179,7 @@
                         {{ __('Se Déconnecter') }}
                     </x-responsive-nav-link>
                 </form>
+
             </div>
         </div>
     </div>
