31b497d18faaad5f9390e2ed8fc0e3396fff0cc5
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-27 10:31:29 +0100

    elearning

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,7 +44,7 @@
 
         <!-- Date de Naissance et Lieu de Naissance -->
         <div class="mt-4">
-            <x-input-label for="date_de_naissance" :value="__('Date de Naissance')" class="text-blue-900 font-semibold flex items-center gap-2">
+            <x-input-label for="date_de_naissance" :value="__('Date de Naissance')"  class="text-blue-900 font-semibold flex items-center gap-2">
                 <svg class="w-5 h-5 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                 {{ __('Date de Naissance') }} & {{ __('Lieu de Naissance') }}
             </x-input-label>
@@ -55,7 +55,7 @@
             <x-input-error :messages="$errors->get('date_de_naissance')" class="mt-2 text-red-600" />
             <x-input-error :messages="$errors->get('lieu_de_naissance')" class="mt-2 text-red-600" />
         </div>
-        
+
         <!-- Sexe -->
         <div class="mt-4">
             <x-input-label for="sexe" :value="__('Sexe')" class="text-blue-900 font-semibold flex items-center gap-2">
