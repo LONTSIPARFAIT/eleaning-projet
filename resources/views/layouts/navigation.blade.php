4ecbaba6e90fb0d490a9716e929c06fe8875ee47
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-27 12:59:24 +0100

    elearning

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,12 +1,12 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 shadow-md">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-20 sm:h-24 items-center">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('welcome') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-blue-800 dark:text-orange-500 transition duration-200" />
+                        <x-application-logo class="block h-9 sm:h-[7rem] w-auto sm:w-[10rem] rounded-[8px] fill-current text-blue-800 dark:text-orange-500 transition duration-200" />
                     </a>
                 </div>
 
