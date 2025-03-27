4040fa5e98a85fdb7b51e384a7b780b0814e18dd
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-27 10:33:14 +0100

    elearning

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,8 @@
 @props(['value'])
 
-<label  {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+    @if (isset($icon))
+        {{ $icon }}
+    @endif
     {{ $value ?? $slot }}
 </label>
