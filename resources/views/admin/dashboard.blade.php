5d7a482adcdd162cde13762a588caa79a86cb9b0
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-28 15:19:26 +0100

    elearning

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -152,7 +152,7 @@
                 </h2>
                 <div class="overflow-x-auto">
                     <table class="w-full text-left border-collapse">
-                        <thead class="bg-red-600 dark:bg-gray-700 text-white">
+                        <thead class="bg-red-600 dark:bg-orange-600 text-white">
                             <tr>
                                 <th class="py-3 px-4 text-sm font-medium rounded-tl-lg">
                                     <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
