4f97a6898826bcc5d07d69c47524b76b271cfddf
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-06 21:54:59 +0100

    elearning

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -54,8 +54,7 @@
                         </div>
                         <a
                             href="{{-- {{ route('course.show', $course->id) }} --}}"
-                            class="mt-4 inline-block bg-red-500 hover:bg-red-600 dark:bg-orange-500 dark:hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded-lg transition-all duration-300 hover:shadow-lg f
-                             items-center gap-2"
+                            class="mt-4 inline-block bg-red-500 hover:bg-red-600 dark:bg-orange-500 dark:hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded-lg transition-all duration-300 hover:shadow-lg f items-center gap-2"
                         >
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z M19 12a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                             Voir le Cours
