45935a63dbbc78e428d7f0c8391fc3bbdbaea251
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-08 20:14:49 +0100

    elearning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <title>CFPC-Learning</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="preconnect" href="https://fonts.bunny.net"> 
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Font Awesome CDN -->
