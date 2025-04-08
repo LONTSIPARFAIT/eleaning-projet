c814b72abaadece0943f1098bf5ba42261cfa323
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-08 20:30:35 +0100

    elearning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,7 @@
                     </svg>
                     <svg x-show="mobileOpen" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12" />
+                          d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
