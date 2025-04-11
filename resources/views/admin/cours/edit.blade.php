8caee04ac5dd4d23cf8467057abebcb148e978f1
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-11 22:31:58 +0100

    elearning

--- resources/views/admin/cours/edit.blade.php
+++ resources/views/admin/cours/edit.blade.php
@@ -52,7 +52,7 @@
             <div class="animate-fade-in-up">
                 <label for="duration" class="block text-blue-900 font-semibold flex items-center gap-2">
                     <svg class="w-5 h-5 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    Durée (minutes)
+                    Durée (Heure)
                 </label>
                 <input
                     type="number"
