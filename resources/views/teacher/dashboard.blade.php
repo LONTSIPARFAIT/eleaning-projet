0b09467c39e48c4c07c06deccd6c83f58a3b4404
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-11 18:53:34 +0100

    elearning

--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -52,13 +52,13 @@
                 </h3>
                 <p class="text-4xl font-bold text-red-600 dark:text-orange-400">{{ $teacherAssignmentsCount ?? 14 }}</p>
             </div>
-            <div class="bg-orange-50 dark:bg-gray-700 shadow-lg rounded-xl p-6 transform transition-all duration-300 hover:scale-105 animate-slide-in-up border border-blue-200 dark:border-orange-600">
+            {{-- <div class="bg-orange-50 dark:bg-gray-700 shadow-lg rounded-xl p-6 transform transition-all duration-300 hover:scale-105 animate-slide-in-up border border-blue-200 dark:border-orange-600">
                 <h3 class="text-lg font-bold text-blue-900 dark:text-orange-400 mb-2 flex items-center gap-2">
                     <svg class="w-6 h-6 text-red-600 dark:text-orange-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     Revenus Générés
                 </h3>
                 <p class="text-4xl font-bold text-red-600 dark:text-orange-400">{{ $teacherRevenue ?? '250 €' }}</p>
-            </div>
+            </div> --}}
         </div>
 
         <!-- Derniers Cours Créés -->
