48cb81e9b25cf0bf2ef984a07ca388eea556186c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-11 18:57:24 +0100

    elearning

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -85,7 +85,7 @@
                                 <p class="text-2xl font-bold text-red-600 dark:text-orange-400 mt-2">{{ $teacherCount }}</p>
                                 <a href="" class="text-red-500 dark:text-orange-500 hover:text-red-600 dark:hover:text-orange-600 text-sm transition duration-300">
                                     <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
-                                    Voir tous les enseignants
+                                    Voir tous les Formateur
                                 </a>
                             </div>
                             {{-- <div class="bg-orange-50 dark:bg-gray-700 rounded-lg p-4 border border-blue-200 dark:border-orange-600 hover:scale-105 transition-all duration-300">
