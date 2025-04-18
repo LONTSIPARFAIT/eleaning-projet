6e22c52df450d66ffd905e230cf800c87be9bac0
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-18 14:20:16 +0100

    elearning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,7 +181,7 @@
                 <a href="#cours"
                     class="bg-red-500 hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-300">
                     <i
-    class="fas fa-book-open mr-2"></i>Découvrir les Cours</a>
+class="fas fa-book-open mr-2"></i>Découvrir les Cours</a>
                 <a href="{{ route('register') }}"
                     class="bg-orange-500 hover:bg-orange-600 dark:bg-orange-600 dark:hover:bg-orange-700 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-300">
                     <i class="fas fa-user-plus mr-2"></i>S'inscrire</a>
