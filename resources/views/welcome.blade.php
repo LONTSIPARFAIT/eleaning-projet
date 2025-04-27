112e2afd437682b7158344f89bcd0fb9b767b96a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-27 20:19:30 +0100

    elearning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
                 </a>
                 <a href="#about"
                     class="text-white hover:text-orange-300 dark:hover:text-orange-400 transition duration-300 relative group">
-                    À 
+                    À p
                     <span
                         class="absolute left-0 bottom-0 w-0 h-1 bg-orange-400 dark:bg-orange-500 transition-all duration-300 group-hover:w-full"></span>
                 </a>
