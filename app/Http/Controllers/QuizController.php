2a76656dc249c5ed26996fadf5110eb58db56d88
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-05-08 10:20:03 +0100

    elearning

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -30,7 +30,7 @@ class QuizController extends Controller
         return view('quizzes.show', compact('quiz'));
     }
 
-    public function submit(Request $re $id)
+    public function submit(Request $req $id)
     {
         $quiz = Quiz::with('questions.answers')->findOrFail($id);
         $score = 0;
